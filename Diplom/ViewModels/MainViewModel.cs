b13e0f5c51d7db6b558ad632d6fac71b0017b6d7
AjStyles2 <71986423+AjStyles2@users.noreply.github.com>
2021-05-25 19:28:49 +0300

    MVVM

---
+++ Diplom/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Diplom.Commands; // подключаем папку Commands для того, чтобы увиделся класс
+
+namespace Diplom.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private BaseViewModel _selectedViewModel;
+        public BaseViewModel SelectedViewModel
+        {
+            get { return _selectedViewModel; }
+            set
+            {
+                _selectedViewModel = value;
+                OnPropertyChanged(nameof(SelectedViewModel));
+            }
+        }
+
+        public ICommand UpdateViewCommand { get; set; }
+
+        public MainViewModel()
+        {
+            UpdateViewCommand = new UpdateViewCommand(this);
+        }
+    }
+}
